e9d30d0cd371a7fb3182579abe9ecab528d41a76
Hunam <hunam@disroot.org>
2023-06-01 01:27:52 +0300

    feat(mod01/ex05): working

---
+++ mod01/ex05/includes/Harl.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#ifndef HARL_HPP
+#define HARL_HPP
+
+#include <string>
+
+class Harl
+{
+private:
+	void debug();
+	void info();
+	void warning();
+	void error();
+
+public:
+	Harl() {}
+	~Harl() {}
+	void complain(std::string level);
+};
+
+#endif
