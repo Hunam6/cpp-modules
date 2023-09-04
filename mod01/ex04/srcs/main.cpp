6320ae2747654596581f2945cd8355d05054acc7
Hunam <hunam@disroot.org>
2023-09-04 18:34:34 +0300

    fix(mod01/ex04): better Makefile, tweaks

--- mod01/ex04/srcs/main.cpp
+++ mod01/ex04/srcs/main.cpp
@@ -14,21 +14,20 @@ std::string getFileContent(char *fileName)
 	return fileContent;
 }
 
-void replaceSubString(std::string &fileContent, char *raw_from, char *raw_to)
+void replaceSubString(std::string &fileContent, std::string from, char *to)
 {
-	std::string from = raw_from;
 	size_t pos = fileContent.rfind(from);
 	while (pos != std::string::npos)
 	{
 		fileContent.erase(pos, from.size());
-		fileContent.insert(pos, raw_to);
+		fileContent.insert(pos, to);
 		pos = fileContent.rfind(from, pos - 1);
 	}
 }
 
-void writeOuputFile(char *fileName, std::string &fileContent)
+void writeOuputFile(std::string fileName, std::string &fileContent)
 {
-	std::ofstream outputFileStream((std::string(fileName) + ".replace").c_str());
+	std::ofstream outputFileStream(fileName + ".replace");
 	if (!outputFileStream)
 		(std::cerr << "Failed to open the output file stream\n", exit(1));
 	outputFileStream << fileContent;
@@ -38,8 +37,8 @@ void writeOuputFile(char *fileName, std::string &fileContent)
 int main(int ac, char **av)
 {
 	if (ac != 4)
-		(std::cerr << "Usage: <filename> <s1> <s2>\n", exit(1));
+		(std::cerr << "Usage: <filename> <s1> <s2>\n", exit(0));
 	std::string fileContent = getFileContent(av[1]);
-	replaceSubString(fileContent, av[2], av[3]);
-	writeOuputFile(av[1], fileContent);
+	replaceSubString(fileContent, std::string(av[2]), av[3]);
+	writeOuputFile(std::string(av[1]), fileContent);
 }
