4dee004218463acd6b0cdbacaf43df2651e8e6c9
Hunam <hunam@disroot.org>
2023-10-07 00:18:00 +0300

    fix(mod06/ex02): tiny tweak

--- mod06/ex02/srcs/main.cpp
+++ mod06/ex02/srcs/main.cpp
@@ -11,11 +11,13 @@ void identify(Base &p);
 int main()
 {
 	Base *random_class = generate();
-	Base *classA = new A();
 	identify(random_class);
 	identify(*random_class);
+
+	Base *classA = new A();
 	identify(classA);
 	identify(*classA);
+
 	delete random_class;
 	delete classA;
 }
